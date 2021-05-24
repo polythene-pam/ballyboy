b8a5fafedd9bfd806cd20da3a8382babbd24978d
Davis Armstrong <davisarmstrong@Daviss-MacBook-Pro-2.local>
2021-05-24 10:01:38 +0100

    choices link

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo2" alt="logo" />
         <p>
-          H3H3
+          <a href="choicesapp://redirectToLogin">Login to choices</a> H3H3
         </p>
       </header>
     </div>
